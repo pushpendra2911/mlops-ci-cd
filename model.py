ef1315d2ee0521499a8e658c243579cb78e559e4
Pushpendra Singh <pushpendrasingh.kharsan@jci.com>
2024-07-29 10:35:50 -0500

    Fix linting issues, add requirements, update workflow

--- model.py
+++ model.py
@@ -2,13 +2,13 @@
 import numpy as np
 from sklearn.linear_model import LogisticRegression
 
-def train_model(X, y):
+def train_model(x, y):
     model = LogisticRegression()
-    model.fit(X, y)
+    model.fit(x, y)
     return model
 
 if __name__ == "__main__":
     X = np.array([[1, 2], [2, 3], [3, 4], [4, 5]])
-    y = np.array([0, 0, 1, 1])
-    model = train_model(X, y)
+    Y = np.array([0, 0, 1, 1])
+    model = train_model(X, Y)
     print("Model trained.")
--- test_model.py
+++ test_model.py
@@ -4,7 +4,11 @@ from model import train_model
 
 def test_train_model():
     X = np.array([[1, 2], [2, 3], [3, 4], [4, 5]])
-    y = np.array([0, 0, 1, 1])
-    model = train_model(X, y)
+    Y = np.array([0, 0, 1, 1])
+    model = train_model(X, Y)
     assert model is not None
     assert model.predict(X).shape == (4,)
+
+if __name__ == "__main__":
+    test_train_model()
+    print("All tests passed.")
